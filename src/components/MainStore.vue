a06b1d87d06395760ec9a4479a0b3932c11ea8fe
Roberto Morin <techgeekmorin@gmail.com>
2025-02-25 21:57:59 -0600

    Pagination Added

--- src/components/MainStore.vue
+++ src/components/MainStore.vue
@@ -1,12 +1,13 @@
 <script setup>
     import MainStoreItem from './MainStoreItem.vue';
-
+    import MainPagination from './MainPagination.vue';
 
 </script>
 
 <template>
     <div class="grid grid-cols-3 gap-10 p-10">
-        <MainStoreItem v-for="item in 15" :key="item"></MainStoreItem>
+        <MainStoreItem v-for="item in 15" :key="item"></MainStoreItem>  
     </div>
+    <MainPagination></MainPagination>
 
 </template>
