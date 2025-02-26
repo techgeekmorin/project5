a7d492a1a54784c47c073e9c2bc09cb063b1ccb7
Roberto Morin <techgeekmorin@gmail.com>
2025-02-25 21:50:50 -0600

    add to cart button added

--- src/views/ItemDescPage.vue
+++ src/views/ItemDescPage.vue
@@ -7,6 +7,7 @@ const route = useRoute()
     <main class="flex flex-col items-center justify-center min-h-screen">
         <p class="text-4xl font-thin text-black">{{ route.params.name }}</p>
         <p class="text-2xl font-thin text-black">{{ route.params.desc }}</p>
+        <button class="bg-green-500 mt-4 px-4 py-2 hover:bg-green-800 hover:text-white">Add to Cart</button>
     </main>
     
 </template>
